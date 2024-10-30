45e147775be8c775c12fd1530d3a58fe103d8781
Jad <jad.saad@aot-technologies.com>
2024-10-30 16:12:03 -0700

    create users table

--- submit-api/src/submit_api/models/user.py
+++ submit-api/src/submit_api/models/user.py
@@ -1,11 +1,10 @@
-"""Account User model class.
+"""User model class.
 
-Manages the account user
+Manages the user
 """
 from __future__ import annotations
 
 from sqlalchemy import Column, Index
-from sqlalchemy.orm import column_property
 
 from .base_model import BaseModel
 from .db import db
@@ -17,12 +16,6 @@ class User(BaseModel):
     __tablename__ = 'users'
 
     id = Column(db.Integer, primary_key=True, autoincrement=True)
-    first_name = Column(db.String(50), nullable=False)
-    last_name = Column(db.String(50), nullable=False)
-    full_name = column_property(first_name + ' ' + last_name)
-    position = Column(db.String(100), nullable=True)
-    work_email_address = Column(db.String(100), nullable=True)
-    work_contact_number = Column(db.String(50), nullable=True)
     auth_guid = Column(db.String(), nullable=False, unique=True)
 
     __table_args__ = (
@@ -33,11 +26,6 @@ class User(BaseModel):
     def create_account_user(cls, data, session=None) -> User:
         """Create account."""
         account_user = User(
-            first_name=data.get('first_name', None),
-            last_name=data.get('last_name', None),
-            position=data.get('position', None),
-            work_email_address=data.get('work_email_address', None),
-            work_contact_number=data.get('work_contact_number', None),
             auth_guid=data.get('auth_guid', None)
         )
         if session:
