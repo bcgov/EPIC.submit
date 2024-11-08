4f70aa1bbda660ea5003a0001f3dfcb286f45aa6
Jad <jad.saad@aot-technologies.com>
2024-11-07 17:06:27 -0800

    Consolidated schema and  service files

--- submit-api/src/submit_api/resources/staff/project.py
+++ submit-api/src/submit_api/resources/staff/project.py
@@ -19,8 +19,8 @@ from flask_restx import Namespace, Resource, cors
 
 from submit_api.auth import auth
 from submit_api.resources.apihelper import Api as ApiHelper
-from submit_api.schemas.staff.account_project import AccountProjectSchema
-from submit_api.services.staff.project_service import ProjectService
+from submit_api.schemas.project import StaffAccountProjectSchema as AccountProjectSchema
+from submit_api.services.project_service import ProjectService
 from submit_api.utils.util import cors_preflight
 
 
