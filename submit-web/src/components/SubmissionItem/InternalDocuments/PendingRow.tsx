c0d3f10ba9f1799fc22af13ae842d15f57736089
Jad <jad.saad@aot-technologies.com>
2024-11-18 09:47:15 -0800

    remove uploadObject dependency in useEffect

--- submit-web/src/components/SubmissionItem/InternalDocuments/PendingRow.tsx
+++ submit-web/src/components/SubmissionItem/InternalDocuments/PendingRow.tsx
@@ -73,7 +73,7 @@ export default function PendingRow({ pendingDocument }: RowProps) {
     if (pendingDocument.pending) {
       uploadObject();
     }
-  }, [pendingDocument.pending, uploadObject]);
+  }, [pendingDocument.pending]);
 
   return (
     <TableRow>
