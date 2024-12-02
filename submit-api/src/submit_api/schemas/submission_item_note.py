2f5da30e10071668ab32a80d38827f042727a841
David Nunez <david-nunez@aot-technologies.com>
2024-12-02 12:55:20 -0500

    set up pre dump

--- submit-api/src/submit_api/schemas/submission_item_note.py
+++ submit-api/src/submit_api/schemas/submission_item_note.py
@@ -3,7 +3,7 @@
 Manages the package
 """
 
-from marshmallow import EXCLUDE, Schema, fields
+from marshmallow import EXCLUDE, Schema, fields, pre_dump
 
 
 class SubmissionItemNote(Schema):
@@ -19,6 +19,12 @@ class SubmissionItemNote(Schema):
     item_id = fields.Int(data_key="item_id")
     created_date = fields.DateTime(data_key="created_date")
     created_by = fields.Str(data_key="created_by")
+
+    @pre_dump
+    def get_submitted_by(self, obj, **kwargs):
+        """Get created_by."""
+        obj.created_by = obj.created_by_user.account_user.full_name if obj.created_by_user else None
+        return obj
 
 
 class PostSubmissionItemNote(Schema):
