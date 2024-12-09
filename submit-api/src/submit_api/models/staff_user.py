35e8a6f2ad6ab9f42c12073e8d403bb73e8defd2
David Nunez <david-nunez@aot-technologies.com>
2024-12-09 16:20:36 -0500

    setup staff user table

---
+++ submit-api/src/submit_api/models/staff_user.py
@@ -0,0 +1,56 @@
+"""Account User model class.
+
+Manages the account user
+"""
+from __future__ import annotations
+
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.orm import column_property
+
+from .base_model import BaseModel
+from .db import db
+from .user import User as UserModel
+
+
+class StaffUser(BaseModel):
+    """Definition of the Account User entity."""
+
+    __tablename__ = 'staff_users'
+
+    id = Column(db.Integer, primary_key=True, autoincrement=True)
+    account_id = Column(db.Integer, ForeignKey('accounts.id'), nullable=False)
+    first_name = Column(db.String(50), nullable=False)
+    last_name = Column(db.String(50), nullable=False)
+    full_name = column_property(first_name + ' ' + last_name)
+    position = Column(db.String(100), nullable=False)
+    work_email_address = Column(db.String(100), nullable=False)
+    work_contact_number = Column(db.String(50), nullable=False)
+    user_id = Column(db.Integer, ForeignKey('users.id'), nullable=False)
+
+    account = db.relationship('Account', foreign_keys=[account_id], lazy='joined')
+    user = db.relationship('User', foreign_keys=[user_id], lazy='joined')
+
+    @classmethod
+    def create_staff_user(cls, data, session=None) -> StaffUser:
+        """Create account."""
+        staff_user = StaffUser(
+            account_id=data.get('account_id', None),
+            first_name=data.get('first_name', None),
+            last_name=data.get('last_name', None),
+            position=data.get('position', None),
+            work_email_address=data.get('work_email_address', None),
+            work_contact_number=data.get('work_contact_number', None),
+            user_id=data.get('user_id', None)
+        )
+        if session:
+            session.add(staff_user)
+            session.commit()
+        else:
+            staff_user.save()
+        return staff_user
+
+    @classmethod
+    def get_by_guid(cls, _guid):
+        """Get account user by guid."""
+        staff_user = cls.query.join(UserModel).filter(UserModel.auth_guid == _guid).first()
+        return staff_user
---
+++ submit-api/src/submit_api/resources/staff/account.py
@@ -0,0 +1,87 @@
+# Copyright © 2024 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""API endpoints for managing an account resource."""
+
+from http import HTTPStatus
+
+from flask_restx import Namespace, Resource, cors
+
+from submit_api.auth import auth
+from submit_api.exceptions import ResourceNotFoundError
+from submit_api.resources.apihelper import Api as ApiHelper
+from submit_api.schemas.account import AccountCreateSchema, AccountSchema
+from submit_api.services.account_service import AccountService
+from submit_api.utils.util import cors_preflight
+
+
+API = Namespace("accounts", description="Endpoints for Account Management")
+"""Custom exception messages
+"""
+
+account_create_model = ApiHelper.convert_ma_schema_to_restx_model(
+    API, AccountCreateSchema(), "Account Create"
+)
+account_list_model = ApiHelper.convert_ma_schema_to_restx_model(
+    API, AccountSchema(), "Account"
+)
+
+
+@cors_preflight("GET, OPTIONS, POST")
+@API.route("", methods=["POST", "GET", "OPTIONS"])
+class Accounts(Resource):
+    """Resource for managing accounts."""
+
+    @staticmethod
+    @API.response(code=HTTPStatus.OK, description="Success", model=[account_list_model])
+    @ApiHelper.swagger_decorators(API, endpoint_description="Fetch all accounts")
+    @auth.require
+    @cors.crossdomain(origin="*")
+    def get():
+        """Fetch all accounts."""
+        accounts = AccountService.get_all_accounts()
+        accounts_list_schema = AccountSchema(many=True)
+        return accounts_list_schema.dump(accounts), HTTPStatus.OK
+
+    @staticmethod
+    @ApiHelper.swagger_decorators(API, endpoint_description="Create an account")
+    @API.expect(account_create_model)
+    @API.response(code=HTTPStatus.CREATED, model=account_list_model, description="Account Created")
+    @API.response(HTTPStatus.BAD_REQUEST, "Bad Request")
+    @auth.require
+    @cors.crossdomain(origin="*")
+    def post():
+        """Create an account."""
+        account_data = AccountCreateSchema().load(API.payload)
+        created_account = AccountService.create_account(account_data)
+        return AccountSchema().dump(created_account), HTTPStatus.CREATED
+
+
+@cors_preflight("GET, OPTIONS")
+@API.route("/proponent/<int:proponent_id>", methods=["GET", "OPTIONS"])
+@API.doc(params={"proponent_id": "The account identifier"})
+class User(Resource):
+    """Resource for managing a single account"""
+
+    @staticmethod
+    @ApiHelper.swagger_decorators(API, endpoint_description="Fetch a account by proponent id")
+    @API.response(code=200, model=account_list_model, description="Success")
+    @API.response(404, "Not Found")
+    @auth.require
+    @cors.crossdomain(origin="*")
+    def get(proponent_id):
+        """Fetch an account by proponent id."""
+        account = AccountService.get_account_by_proponent_id(proponent_id)
+        if not account:
+            return ResourceNotFoundError(f"Account with proponent {proponent_id} not found")
+        return AccountSchema().dump(account), HTTPStatus.OK
