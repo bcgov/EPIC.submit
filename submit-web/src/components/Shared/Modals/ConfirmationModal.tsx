bd144aa3e14c5c1a48b36b2b044a00cde9e43392
David Nunez <david-nunez@aot-technologies.com>
2024-12-31 10:51:35 -0500

    update modal design

--- submit-web/src/components/Shared/Modals/ConfirmationModal.tsx
+++ submit-web/src/components/Shared/Modals/ConfirmationModal.tsx
@@ -5,10 +5,14 @@ import {
   DialogTitle,
   Button,
   Box,
+  Typography,
+  IconButton,
+  Divider,
 } from "@mui/material";
 import { useModal } from "./modalStore";
 import { modalStyle } from "./constants";
 import { LoadingButton } from "../LoadingButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 type ConfirmationModalProps = {
   title: string;
@@ -30,11 +34,21 @@ const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
   const { setClose } = useModal();
   return (
     <Box sx={modalStyle}>
-      <DialogTitle>{title}</DialogTitle>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <DialogTitle>{title}</DialogTitle>
+        <IconButton onClick={setClose}>
+          <CloseIcon />
+        </IconButton>
+      </Box>
+      <Divider />
       <DialogContent>
-        <DialogContentText>{description}</DialogContentText>
+        <Typography variant="body1">{description}</Typography>
       </DialogContent>
+      <Divider />
       <DialogActions sx={{ padding: "1rem" }}>
+        <Button onClick={setClose} color="secondary" sx={{ border: 0 }}>
+          {cancelText ?? "Cancel"}
+        </Button>
         <LoadingButton
           loading={loading}
           variant="contained"
@@ -43,9 +57,6 @@ const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
         >
           {confirmText ?? "Confirm"}
         </LoadingButton>
-        <Button onClick={setClose} color="secondary">
-          {cancelText ?? "Cancel"}
-        </Button>
       </DialogActions>
     </Box>
   );
