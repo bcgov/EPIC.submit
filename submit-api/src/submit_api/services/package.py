a778bbfe4ddef270473108237b10679936ee343b
David Nunez <david-nunez@aot-technologies.com>
2025-01-06 12:17:44 -0500

    add missing docstring

--- submit-api/src/submit_api/services/package.py
+++ submit-api/src/submit_api/services/package.py
@@ -135,6 +135,7 @@ class PackageService:
 
     @classmethod
     def get_all_package_versions(cls, package_id):
+        """Get all package versions by package ID."""
         package_version = PackageVersionModel.get_by_package_id(package_id)
         all_package_versions = PackageVersionModel.get_all_by_original_package_id(package_version.original_package_id)
         sorted_package_versions = sorted(all_package_versions, key=lambda x: x.version, reverse=True)
