e55b40b3487fdf1ce8e30505faa84552ca625e6f
Jad <jad.saad@aot-technologies.com>
2025-01-17 11:08:38 -0800

    remove unused import

--- submit-api/src/submit_api/schemas/item.py
+++ submit-api/src/submit_api/schemas/item.py
@@ -9,7 +9,7 @@ from submit_api.enums.item_status import ItemStatus
 from submit_api.models.submission import SubmissionType, SubmissionStatus
 from submit_api.schemas.internal_staff_document import InternalStaffDocument
 from submit_api.schemas.item_type import ItemTypeSchema
-from submit_api.schemas.submission import SubmittedDocumentSchema, SubmittedFormSchema, SubmissionSchema
+from submit_api.schemas.submission import SubmittedDocumentSchema, SubmittedFormSchema
 from submit_api.schemas.submission_item_note import SubmissionItemNote
 from submit_api.schemas.submission_review import SubmissionReviewSchema
 
