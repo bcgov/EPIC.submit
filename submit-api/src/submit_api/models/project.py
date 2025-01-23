506a86d7c93488b33b8e49d7322f0ec2a08690b4
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2025-01-22 20:57:36 -0800

    changed project model (#255)

--- submit-api/src/submit_api/models/project.py
+++ submit-api/src/submit_api/models/project.py
@@ -4,10 +4,7 @@ Manages the account project
 """
 from __future__ import annotations
 
-from sqlalchemy import Column, event
-from sqlalchemy.exc import ArgumentError, IntegrityError
-
-from submit_api.exceptions import PermissionDeniedError
+from sqlalchemy import Column
 
 from .db import db
 
@@ -28,10 +25,6 @@ class Project(db.Model):
         db.Index('ix_projects_proponent_id', 'proponent_id'),
     )
 
-    def __init__(self, **kwargs):
-        """Initialize the Project entity."""
-        raise ArgumentError("Project is read-only, cannot create new instances.")
-
     @classmethod
     def get_all_projects_in_ids(cls, project_ids):
         """Get all projects in the given project ids."""
@@ -41,33 +34,3 @@ class Project(db.Model):
     def get_one_by_proponent_id(cls, proponent_id):
         """Fetch project by proponent id."""
         return cls.query.filter_by(proponent_id=proponent_id).first()
-
-
-@event.listens_for(Project, 'before_insert')
-def before_insert():
-    """Raise an error when trying to insert into the Project table."""
-    raise IntegrityError(
-        "Insertions are not allowed on this table",
-        params=None,
-        orig=PermissionDeniedError('Insertions are not allowed on this table')
-    )
-
-
-@event.listens_for(Project, 'before_update')
-def before_update():
-    """Raise an error when trying to update the Project table."""
-    raise IntegrityError(
-        "Updates are not allowed on this table",
-        params=None,
-        orig=PermissionDeniedError('Updates are not allowed on this table')
-    )
-
-
-@event.listens_for(Project, 'before_delete')
-def before_delete():
-    """Raise an error when trying to delete from the Project table."""
-    raise IntegrityError(
-        "Deletions are not allowed on this table",
-        params=None,
-        orig=PermissionDeniedError('Deletions are not allowed on this table')
-    )
