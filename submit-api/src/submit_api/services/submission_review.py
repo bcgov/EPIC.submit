1caba853d702134e0deea89256ab50630e5bacd1
David Nunez <david.nunez@aot-technologies.com>
2025-01-28 13:48:41 -0500

    removed created status

--- submit-api/src/submit_api/services/submission_review.py
+++ submit-api/src/submit_api/services/submission_review.py
@@ -316,7 +316,7 @@ class SubmissionReviewService:
         if not new_item:
             current_app.logger.error(f"{item.type.name} item not found in new package {new_package.id}.")
             raise ResourceNotFoundError(f"{item.type.name} item not found in new package {new_package.id}.")
-
+        new_package.status = []
         session.add(new_package)
         session.flush()
         current_app.logger.info(f"New package version created for {new_package.name}.")
