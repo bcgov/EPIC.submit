877454befe6824533e6eafc3c16169f25b623d92
David Nunez <103138766+djnunez-aot@users.noreply.github.com>
2025-01-30 10:16:34 -0500

    set up table ui updates for eao

--- submit-web/src/components/Projects/ProjectTable/StaffTableRow.tsx
+++ submit-web/src/components/Projects/ProjectTable/StaffTableRow.tsx
@@ -48,13 +48,14 @@ export default function StaffTableRow({ submissionPackage }: ProjectRowProps) {
 
   return (
     <>
-      <StyledProjectTableRow sx={{ maxHeight: "40px", py: 0 }}>
+      <StyledProjectTableRow>
         <StyledProjectTableCell
           sx={{
             minWidth: "150px",
             flexGrow: 1,
             py: 0,
           }}
+          align="left"
         >
           <SubmitLink
             sx={{
@@ -78,7 +79,7 @@ export default function StaffTableRow({ submissionPackage }: ProjectRowProps) {
           </SubmitLink>
         </StyledProjectTableCell>
         <StyledProjectTableCell
-          align="right"
+          align="left"
           sx={{
             maxWidth: "75px",
             overflow: "hidden",
@@ -89,12 +90,10 @@ export default function StaffTableRow({ submissionPackage }: ProjectRowProps) {
           {type}
         </StyledProjectTableCell>
         <StyledProjectTableCell
-          align="right"
+          align="left"
           sx={{
-            maxWidth: "80px",
             lineHeight: 1.2,
             wordWrap: "break-word",
-            textAlign: "right",
             color: BCDesignTokens.typographyFontSizeBody,
           }}
         >
