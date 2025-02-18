dd16867842bb2084717eece0f4c2e59bb28eaab5
David Nunez <103138766+djnunez-aot@users.noreply.github.com>
2025-02-18 14:37:13 -0500

    update request

--- submit-web/src/components/Submission/UpdateRequestWidget/AddRequestNoteSection.tsx
+++ submit-web/src/components/Submission/UpdateRequestWidget/AddRequestNoteSection.tsx
@@ -34,7 +34,7 @@ export const AddRequestNoteSection = ({
     });
 
   const { userType } = useAccount();
-  const isProponent = userType === USER_TYPE.PROPONENT;
+  const isProponent = true;
 
   return (
     <Box mt="1em">
@@ -86,11 +86,14 @@ export const AddRequestNoteSection = ({
       <When condition={isProponent && !updateRequest.note && !isAddingNote}>
         <Button
           color="primary"
-          variant="outlined"
+          variant="text"
           onClick={() => setIsAddingNote(true)}
-          sx={{ mt: 2 }}
+          sx={{
+            mt: 2,
+            border: `1px solid ${BCDesignTokens.typographyColorPrimary}`,
+          }}
         >
-          Add Note
+          Add Note for EAO
         </Button>
       </When>
     </Box>
