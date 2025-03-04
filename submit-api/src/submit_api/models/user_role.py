12bc58fef5dc0c9162ffe8c5b938cb23e2c7d1a8
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2025-03-04 12:52:56 -0800

    changed data model to role/user

---
+++ submit-api/src/submit_api/models/user_role.py
@@ -0,0 +1,35 @@
+"""Mode for user role."""
+from __future__ import annotations
+
+from sqlalchemy import Column, Integer, ForeignKey
+
+from .base_model import BaseModel
+
+
+class UserRole(BaseModel):
+    """Table to manage user role assignments."""
+
+    __tablename__ = "user_roles"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    account_user_id = Column(Integer, ForeignKey("account_users.id"), nullable=False)
+    account_project_id = Column(Integer, ForeignKey("account_projects.id"),
+                                nullable=True)  # NULL for account-wide roles
+    package_id = Column(Integer, ForeignKey("packages.id"), nullable=True)  # NULL for project-wide roles
+    role_id = Column(Integer, ForeignKey("roles.id"), nullable=False)
+
+    @classmethod
+    def create_user_role(cls, data, session=None) -> UserRole:
+        """Create a user role assignment."""
+        user_role = UserRole(
+            account_user_id=data.get("account_user_id"),
+            account_project_id=data.get("account_project_id"),
+            submission_id=data.get("submission_id"),
+            role_id=data.get("role_id"),
+        )
+        if session:
+            session.add(user_role)
+            session.commit()
+        else:
+            user_role.save()
+        return user_role
