642e89d150b860daf19a0a30808a195b2a2324d9
Jad <jad.saad@aot-technologies.com>
2025-03-11 16:07:13 -0700

    Add search filter for the entity table

--- submit-web/src/routes/staff/_staffLayout/user-management/index.tsx
+++ submit-web/src/routes/staff/_staffLayout/user-management/index.tsx
@@ -1,8 +1,9 @@
 import { ContentBox } from "@/components/Shared/ContentBox";
-import { PageGrid } from "@/components/Shared/PageGrid";
 import { EntityTable } from "@/components/UserManagement/staff/EntityTable";
-import { Grid, Typography } from "@mui/material";
+import { useEntityTable } from "@/components/UserManagement/staff/EntityTable/entityTableStore";
+import { Box, Grid, TextField, Typography } from "@mui/material";
 import { createFileRoute } from "@tanstack/react-router";
+import SearchIcon from "@mui/icons-material/Search";
 
 export const Route = createFileRoute("/staff/_staffLayout/user-management/")({
   component: UserManagement,
@@ -10,20 +11,44 @@ export const Route = createFileRoute("/staff/_staffLayout/user-management/")({
 });
 
 function UserManagement() {
+  const { searchText, setSearchText } = useEntityTable();
+
   return (
-    <PageGrid>
-      <Grid item xs={12}>
-        <ContentBox
-          mainLabel="Generate URL"
-          sx={{ width: "100%", height: "fit-content" }}
-          contentBoxVariant="secondary"
-        >
-          <Typography variant="subtitle1">
-            Select a Certificate Holder and/or Exemption Holder
-          </Typography>
-          <EntityTable sx={{ marginTop: "2em" }} />
-        </ContentBox>
+    <Box
+      sx={{
+        padding: "36px 24px",
+        width: "100%",
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            value={searchText}
+            variant="outlined"
+            onChange={(e) => setSearchText(e.target.value)}
+            sx={{
+              margin: 0,
+              width: "398px",
+            }}
+            placeholder="Search Holders by Name"
+            InputProps={{
+              startAdornment: <SearchIcon htmlColor="#858a8c" />,
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <ContentBox
+            mainLabel="Generate URL"
+            sx={{ width: "100%", height: "fit-content" }}
+            contentBoxVariant="secondary"
+          >
+            <Typography variant="subtitle1">
+              Select a Certificate Holder and/or Exemption Holder
+            </Typography>
+            <EntityTable sx={{ marginTop: "2em" }} />
+          </ContentBox>
+        </Grid>
       </Grid>
-    </PageGrid>
+    </Box>
   );
 }
