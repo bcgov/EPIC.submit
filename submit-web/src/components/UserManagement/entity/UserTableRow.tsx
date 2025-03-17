83cfde57dbf97a9d63e91f72cbf33009bfa34c9a
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2025-03-17 15:09:37 -0700

    removing hard coded text

--- submit-web/src/components/UserManagement/entity/UserTableRow.tsx
+++ submit-web/src/components/UserManagement/entity/UserTableRow.tsx
@@ -7,12 +7,13 @@ import { roleDetails } from "@/models/Role";
 import { useNavigate } from "@tanstack/react-router";
 import { useUserStore } from './userStore';
 import { notify } from "../../Shared/Snackbar/snackbarStore";
+import { InvitationStatus } from "@/models/Invitation";
 
 export default function UserTableRow({ user }: { user: AccountUserWithRole }) {
   const { setSelectedUser } = useUserStore();
   const navigate = useNavigate();
   const onUserClick = () => {
-    if (user.status === "pending") {
+    if (user.status === InvitationStatus.PENDING) { 
       notify.error("User is still invited and details cannot be viewed yet.");
       return;
     }
