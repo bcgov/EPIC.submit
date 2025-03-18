40c61480f8ba70241b6a3bc906486255d6a5ee92
Jad <jad.saad@aot-technologies.com>
2025-03-18 15:07:07 -0700

    remove overridden method

--- submit-api/src/submit_api/models/account_project.py
+++ submit-api/src/submit_api/models/account_project.py
@@ -63,9 +63,3 @@ class AccountProject(BaseModel):
         else:
             account_project.save()
         return account_project
-
-    @classmethod
-    def find_by_id(cls, identifier):
-        """Find account project by id."""
-        # use the query module to get the account project by id
-        raise NotImplementedError
