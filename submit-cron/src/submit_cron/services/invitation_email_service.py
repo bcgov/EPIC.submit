7dfe5009f9ad80d4c5c12fd20881bb647522c0d4
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2025-04-09 14:09:44 -0700

    fixed issues with email

--- submit-cron/src/submit_cron/services/invitation_email_service.py
+++ submit-cron/src/submit_cron/services/invitation_email_service.py
@@ -30,15 +30,13 @@ class InvitationEmailService:  # pylint: disable=too-few-public-methods
         project_name = None
 
         if invitation.project_ids:
-            project_name = cls.get_project_names(invitation.project_ids)
+            project = cls.get_project_from_project_ids(invitation.project_ids)
         elif invitation.package_ids:
-            project_name = cls.get_project_names_for_package_id(invitation.package_ids)
+            project = cls.get_project_from_package_id(invitation.package_ids)
         elif invitation.account_id:
             project = cls.get_project_for_account_id(invitation.account_id)
-            if project:
-                project_name = project.name
 
-        if not project_name and not project:
+        if not project:
             raise BadRequestError(f"Project was not found for invitation id: {invitation.id}")
 
         invitation_url = cls.generate_signup_url(invitation.token)
@@ -48,9 +46,9 @@ class InvitationEmailService:  # pylint: disable=too-few-public-methods
             body_args={
                 'epic_submit_link': current_app.config.get('WEB_URL'),
                 'invitation_url': invitation_url,
-                'project_name': project_name if project_name else project.name,
+                'project_name': project.name or '',
                 'bc_service_card_url': bc_service_card_url,
-                'certificate_holder_name': project.proponent_name if project.proponent_name else '',
+                'certificate_holder_name': project.proponent_name or '',
                 'invitation_action_text': invitation_action_text,
             },
             subject='Invitation to collaborate on EPIC.submit',
@@ -61,46 +59,43 @@ class InvitationEmailService:  # pylint: disable=too-few-public-methods
         return email_details
 
     @staticmethod
-    def get_project_names(project_ids: str) -> str:
-        """Fetch project names as a comma-separated string for given project IDs."""
+    def get_project_from_project_ids(project_ids: str) -> ProjectModel:
+        """Return the first matching project from a list of project IDs."""
         project_id_list = [int(pid) for pid in project_ids if isinstance(pid, (int, str)) and str(pid).isdigit()]
-
-        projects = db.session.query(ProjectModel.name).filter(ProjectModel.id.in_(project_id_list)).all()
-
-        return ", ".join(p.name for p in projects)
+        return (
+            db.session.query(ProjectModel)
+            .filter(ProjectModel.id.in_(project_id_list))
+            .first()
+        )
 
     @staticmethod
-    def get_project_names_for_package_id(package_ids: list) -> str:
-        """Fetch project names as a comma-separated string for given package IDs."""
+    def get_project_from_package_id(package_ids: list) -> ProjectModel:
+        """Return the project linked to the first package ID."""
         if not isinstance(package_ids, list) or not package_ids:
-            return ""
+            return None
 
-        package_id = package_ids[0]  # we dont have multiple projects so far , so just take any package id
+        package_id = package_ids[0]
 
-        project_name = (
-            db.session.query(ProjectModel.name)
+        return (
+            db.session.query(ProjectModel)
             .join(AccountProjectModel, ProjectModel.id == AccountProjectModel.project_id)
             .join(PackageModel, AccountProjectModel.id == PackageModel.account_project_id)
             .filter(PackageModel.id == package_id)
-            .scalar()
+            .first()
         )
-
-        return project_name or ""
 
     @staticmethod
     def get_project_for_account_id(account_id: int) -> ProjectModel:
-        """Fetch the full ProjectModel instance for a given account ID."""
+        """Return the first project for a given account ID."""
         if not account_id:
             return None
 
-        project = (
+        return (
             db.session.query(ProjectModel)
             .join(AccountProjectModel, ProjectModel.id == AccountProjectModel.project_id)
             .filter(AccountProjectModel.account_id == account_id)
             .first()
         )
-
-        return project
 
     @staticmethod
     def generate_signup_url(token):
