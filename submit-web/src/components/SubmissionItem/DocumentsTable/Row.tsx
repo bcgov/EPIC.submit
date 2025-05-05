d074104b5d40e2a5cdbc1f544d59b775b102dffd
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2025-05-05 14:57:13 -0700

    Fix incorrect S3 folder path used during document upload

--- submit-web/src/components/SubmissionItem/DocumentsTable/Row.tsx
+++ submit-web/src/components/SubmissionItem/DocumentsTable/Row.tsx
@@ -8,6 +8,10 @@ import { useReplaceSubmussion } from "@/hooks/api/useSubmissions";
 import { useParams } from "@tanstack/react-router";
 import { saveObject } from "@/hooks/api/useObjectStorage";
 import { FileUploadButton } from "@/components/Shared/FileUploadButton";
+import {
+  MANAGEMENT_PLAN_DOCUMENT_FOLDERS
+} from "./../ManagementPlanSubmission/ManagementPlanProponentView/constants";
+import { S3_FOLDER } from "@/hooks/api/useObjectStorage";
 
 type DocumentRowProps = Readonly<{
   documentSubmission: Submission;
@@ -63,11 +67,15 @@ export default function Row({ documentSubmission, folderPath }: DocumentRowProps
     const fileToUpload = files[0];
     try {
       setIsReplacingDocument(true);
+      const resolvedFolderPath =
+        currentSubmission.submitted_document.folder === MANAGEMENT_PLAN_DOCUMENT_FOLDERS.SUPPORTING
+          ? `${folderPath}${S3_FOLDER.SUPPORTING_DOCUMENTS}`
+          : folderPath;
       const uploadedFile = await saveObject({
         file: fileToUpload,
         fileDetails: {
           filename: fileToUpload.name,
-          folder: folderPath,
+          folder: resolvedFolderPath,
         },
       });
 
