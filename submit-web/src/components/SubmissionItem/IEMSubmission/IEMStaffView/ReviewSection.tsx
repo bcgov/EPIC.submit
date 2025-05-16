0aa9a37a5b3a3c057d8849d0a58ff0b8bbbbb047
David Nunez <david.nunez@aot-technologies.com>
2025-05-16 11:31:25 -0400

    small changes to notes

--- submit-web/src/components/SubmissionItem/IEMSubmission/IEMStaffView/ReviewSection.tsx
+++ submit-web/src/components/SubmissionItem/IEMSubmission/IEMStaffView/ReviewSection.tsx
@@ -41,7 +41,7 @@ type iemReviewForm = yup.InferType<typeof iemReviewSchema>;
 
 const getAnswersByType = (
   review: SubmissionReview,
-  type: SubmissionReviewEntryType,
+  type: SubmissionReviewEntryType
 ) => {
   if (!review?.entries) return {};
   return review.entries?.find((entry) => entry.type === type)?.entry;
@@ -59,7 +59,7 @@ export default function ReviewSection() {
   const queryClient = useQueryClient();
   const submissionItem = queryClient.getQueryData<SubmissionItem>(
     getSubmissionItemForStaffQueryOptions({ itemId: Number(submissionItemId) })
-      .queryKey,
+      .queryKey
   );
 
   const defaultValues = useMemo(() => {
@@ -68,11 +68,11 @@ export default function ReviewSection() {
     const review = submissionItem.review;
     const staffAnswers = getAnswersByType(
       review,
-      SUBMISSION_REVIEW_ENTRY_TYPE.STAFF_RECOMMENDATION,
+      SUBMISSION_REVIEW_ENTRY_TYPE.STAFF_RECOMMENDATION
     );
     const managerAnswers = getAnswersByType(
       review,
-      SUBMISSION_REVIEW_ENTRY_TYPE.MANAGER_CONFIRMATION,
+      SUBMISSION_REVIEW_ENTRY_TYPE.MANAGER_CONFIRMATION
     );
 
     return {
@@ -135,8 +135,10 @@ export default function ReviewSection() {
                 bgcolor: BCDesignTokens.themeBlue60,
                 width: 1,
                 my: BCDesignTokens.layoutMarginXsmall,
+                mb: BCDesignTokens.layoutMarginMedium,
               }}
             />
+            <NotesSection />
             <Typography
               variant="body1"
               sx={{ fontWeight: BCDesignTokens.typographyFontWeightsBold }}
@@ -192,7 +194,6 @@ export default function ReviewSection() {
                 </ControlledRadioGroup>
               </>
             </PermissionsGate>
-            <NotesSection />
             <When condition={failedIEM}>
               <AccordionSummary
                 expandIcon={null}
