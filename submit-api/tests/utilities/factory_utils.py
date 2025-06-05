5c7cc68f47dcce046657a53fc3292e1b72ef1a9b
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2025-06-04 22:12:11 -0700

    SUBMIT-531 Added some tests (#554)

--- submit-api/tests/utilities/factory_utils.py
+++ submit-api/tests/utilities/factory_utils.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Test Utils.
 
 Test Utility for creating model factory.
@@ -22,7 +23,13 @@ from faker import Faker
 from flask import g
 
 from src.submit_api.config import get_named_config
-
+from submit_api.models import db
+from submit_api.models.account import Account
+from submit_api.models.account_project import AccountProject
+from submit_api.models.invitations import InvitationStatus
+from submit_api.models.invitations import Invitations
+from submit_api.models.project import Project
+from submit_api.models.user import UserType
 
 CONFIG = get_named_config("testing")
 fake = Faker()
@@ -48,5 +55,82 @@ def factory_auth_header(jwt, claims):
 
 def generate_abbreviation(number_of_characters):
     """Create abbreviation with given number of characters."""
-    # Create a random 4-letter abbreviation
     return "".join(random.choices(string.ascii_uppercase, k=number_of_characters))
+
+
+def factory_project_model(name="Test Project", proponent_id=1234, proponent_name="Test Proponent"):
+    """Factory project model."""
+    project = Project(
+        name=name,
+        proponent_id=proponent_id,
+        proponent_name=proponent_name,
+        ea_certificate=None,
+        epic_guid=None
+    )
+    db.session.add(project)
+    db.session.commit()
+    return project
+
+
+def factory_account_model(proponent_id=1234):
+    """Factory account model."""
+    account = Account(proponent_id=proponent_id)
+    db.session.add(account)
+    db.session.commit()
+    return account
+
+
+def factory_user_model(auth_guid=None, user_type=UserType.STAFF, session=None):
+    """Factory user model."""
+    from submit_api.models.user import User
+
+    user = User(
+        auth_guid=auth_guid or fake.uuid4(),
+        type=user_type,
+        status_id=1
+    )
+    if session:
+        session.add(user)
+        session.flush()
+    else:
+        user.save()
+    return user
+
+
+def factory_account_project_model(account_id, project_id):
+    """Factory account project model."""
+    account_project = AccountProject(account_id=account_id, project_id=project_id)
+    db.session.add(account_project)
+    db.session.commit()
+    return account_project
+
+
+def factory_project_with_proponent(**kwargs):
+    """Factory project."""
+    project = Project(
+        name=kwargs.get("name", fake.company()),
+        proponent_id=kwargs.get("proponent_id", fake.random_int(min=1000, max=9999)),
+        proponent_name=kwargs.get("proponent_name", fake.company()),
+        ea_certificate=kwargs.get("ea_certificate", fake.uuid4()),
+        epic_guid=kwargs.get("epic_guid", fake.uuid4()),
+    )
+    db.session.add(project)
+    db.session.commit()
+    return project
+
+
+def factory_invitation_model(account_id, status=InvitationStatus.PENDING.value, project_ids=None, package_ids=None,
+                             email=None, role_id=None):
+    """Create and return a mock invitation entry."""
+    invitation = Invitations(
+        account_id=account_id,
+        token=fake.uuid4(),
+        project_ids=project_ids or [1],
+        package_ids=package_ids or [],
+        email=email or fake.email(),
+        status=status,
+        role_id=role_id
+    )
+    db.session.add(invitation)
+    db.session.commit()
+    return invitation
