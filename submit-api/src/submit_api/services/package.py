1a87dce47c92398ebdadb838a4d6e609baa383a1
David Nunez <david.nunez@aot-technologies.com>
2025-06-23 16:23:35 -0400

    linting

--- submit-api/src/submit_api/services/package.py
+++ submit-api/src/submit_api/services/package.py
@@ -103,7 +103,6 @@ class PackageService:
                 new_package.id,
                 MANAGEMENT_PLAN_RESUBMISSION_REQUEST_EMAIL_TEMPLATE
             )
-            
             # Set up the submitter information from the original package
             if original_package.submitted_by:
                 # Get the user by auth_guid
