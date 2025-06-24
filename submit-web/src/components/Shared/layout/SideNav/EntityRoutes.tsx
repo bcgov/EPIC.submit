de7e7671738c042ed4e17aa4d864837deced5c1c
djnunez-aot <103138766+djnunez-aot@users.noreply.github.com>
2025-06-24 14:02:46 -0400

    only display admin tab for project admins (#598)

--- submit-web/src/components/Shared/layout/SideNav/EntityRoutes.tsx
+++ submit-web/src/components/Shared/layout/SideNav/EntityRoutes.tsx
@@ -1,10 +1,17 @@
-import { ACCOUNT_USER_PERMISSIONS } from "@/models/Role";
+import { ACCOUNT_USER_PERMISSIONS, USER_MANAGEMENT_ROLE } from "@/models/Role";
 import PermissionsGate from "../../PermissionGate";
 import { MainListItem } from "./MainListItem";
 import ProjectsSubRoutes from "./ProjectsSubRoutes";
 import { SubListItem } from "./SubListItem";
+import { useAccount } from "@/store/accountStore";
+import { When } from "react-if";
 
 export default function EntityRoutes() {
+  const account = useAccount();
+  const isAdmin =
+    account?.userManagementRole?.role_name ===
+    USER_MANAGEMENT_ROLE.PROJECT_ADMIN;
+
   return (
     <>
       <MainListItem
@@ -14,12 +21,14 @@ export default function EntityRoutes() {
         }}
       />
       <ProjectsSubRoutes />
-      <MainListItem
-        route={{
-          name: "Admin",
-          path: "/proponent/profile",
-        }}
-      />
+      <When condition={isAdmin}>
+        <MainListItem
+          route={{
+            name: "Admin",
+            path: "/proponent/profile",
+          }}
+        />
+      </When>
       <PermissionsGate scopes={[ACCOUNT_USER_PERMISSIONS.INVITE_USERS]}>
         <SubListItem
           key={`sub-list-user-management`}
