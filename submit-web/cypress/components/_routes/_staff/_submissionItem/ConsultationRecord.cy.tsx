aafc529aaf69760477d6cb0f256bc06cf1cd0f83
jadmsaadaot <jad.saad@aot-technologies.com>
2025-07-21 12:59:03 -0700

    SUBMIT-588 Adds Cypress tests for Staff Consultation Records (#651)
    * Adds SubmissionItem Cypress component test
    
    Adds a Cypress component test for the SubmissionItem route.
    
    This test verifies that the page renders correctly and that the review section is visible. It also includes a test case for when the consultation check is completed, ensuring that the relevant notification is displayed.
    
    Also moves the test constants to separate file for reusability.
    
    * Moves SubmissionItem to staff directory
    
    Relocates the SubmissionItem component to the _staff directory
    to better organize staff-specific components.
    
    Addresses issue of component placement within the project structure.
    
    Fixes #587
    
    * Uses staff account for internal documents
    
    Ensures that the tests for internal documents use a staff account mock, reflecting the intended user role for this functionality.
    This change aligns the tests with the actual user authentication context required for accessing and managing internal staff documents.
    
    * Moves internal documents component
    
    Moves the internal documents component to the staff directory.
    
    * Adds staff user authentication roles
    
    Adds mock authentication roles for staff users in the consultation record component test.
    
    This allows the test to properly simulate a staff user with the appropriate permissions, which is necessary to test the intended functionality of the review section.
    
    * Test review section for staff who is not manager
    
    * Renames and updates consultation record test
    
    Renames the submission item test file to more accurately reflect its purpose, focusing on consultation record functionality.
    
    Updates the test suite to distinguish between manager and staff roles when testing the review section.

---
+++ submit-web/cypress/components/_routes/_staff/_submissionItem/ConsultationRecord.cy.tsx
@@ -0,0 +1,239 @@
+import { mount } from "cypress/react18";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "react-oidc-context";
+import { OidcConfig } from "../../../../../src/utils/config";
+import { mockZustandStore, setupTokenStorage } from "../../../utils";
+import { useAccount } from "../../../../../src/store/accountStore";
+import { USER_TYPE } from "../../../../../src/models/User";
+import { QUERY_KEY } from "../../../../../src/hooks/api/constants";
+import { usePackageTableStore } from "../../../../../src/components/Submission/packageTableStore";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "../../../../../src/routeTree.gen";
+import {
+  mockStaffAccount,
+  mockAccountProject,
+  mockAuthentication,
+  mockConsultationRecord,
+  mockSubmissionPackage,
+  mockConsultationRecordItemPassed,
+} from "../../../utils/mockConstants";
+import { EPIC_SUBMIT_ROLE } from "../../../../../src/models/Role";
+
+const mountDefaultPage = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+  queryClient.setQueryData(
+    [QUERY_KEY.SUBMISSION_PACKAGE, mockSubmissionPackage.id],
+    mockSubmissionPackage,
+  );
+  queryClient.setQueryData(
+    [QUERY_KEY.ACCOUNT_PROJECT, mockAccountProject.id],
+    mockAccountProject,
+  );
+  queryClient.setQueryData(
+    [QUERY_KEY.SUBMISSION_ITEM, mockConsultationRecord.id],
+    mockConsultationRecord,
+  );
+
+  const router = createRouter({
+    routeTree: routeTree,
+    context: {
+      authentication: mockAuthentication,
+      queryClient: queryClient,
+      account: mockStaffAccount,
+    },
+  });
+
+  router.navigate({
+    to: `/staff/projects/${mockAccountProject.id}/submission-packages/${mockSubmissionPackage.id}/submissions/${mockConsultationRecord.id}`,
+  });
+
+  mount(
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider {...OidcConfig}>
+        <RouterProvider
+          router={router}
+          context={{
+            authentication: mockAuthentication,
+            account: mockStaffAccount,
+          }}
+        />
+        ;
+      </AuthProvider>
+    </QueryClientProvider>,
+  );
+};
+
+describe("Submission Item Consultation Record Page", () => {
+  beforeEach(() => {
+    cy.viewport(1280, 800);
+    mockZustandStore(useAccount, {
+      userType: USER_TYPE.STAFF,
+      reset: () => {},
+    });
+    mockZustandStore(usePackageTableStore, {
+      isValidating: false,
+      reset: () => {},
+    });
+
+    setupTokenStorage();
+  });
+
+  it("test page renders", () => {
+    mountDefaultPage();
+
+    cy.contains("Consultation Records Information").should("exist");
+    cy.get("[data-testid='review-section']").should("be.visible");
+  });
+
+  it("test review section - manager", () => {
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
+
+    queryClient.setQueryData(
+      [QUERY_KEY.SUBMISSION_PACKAGE, mockSubmissionPackage.id],
+      mockSubmissionPackage,
+    );
+    queryClient.setQueryData(
+      [QUERY_KEY.ACCOUNT_PROJECT, mockAccountProject.id],
+      mockAccountProject,
+    );
+    queryClient.setQueryData(
+      [QUERY_KEY.SUBMISSION_ITEM, mockConsultationRecordItemPassed.id],
+      mockConsultationRecordItemPassed,
+    );
+
+    const router = createRouter({
+      routeTree: routeTree,
+      context: {
+        authentication: mockAuthentication,
+        queryClient: queryClient,
+        account: mockStaffAccount,
+      },
+    });
+
+    mockZustandStore(useAccount, {
+      userType: USER_TYPE.STAFF,
+      roles: [
+        EPIC_SUBMIT_ROLE.extended_eao_edit,
+        EPIC_SUBMIT_ROLE.eao_create,
+        EPIC_SUBMIT_ROLE.eao_edit,
+        EPIC_SUBMIT_ROLE.eao_view,
+      ],
+      reset: () => {},
+    });
+
+    router.navigate({
+      to: `/staff/projects/${mockAccountProject.id}/submission-packages/${mockSubmissionPackage.id}/submissions/${mockConsultationRecordItemPassed.id}`,
+    });
+
+    mount(
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider {...OidcConfig}>
+          <RouterProvider
+            router={router}
+            context={{
+              authentication: mockAuthentication,
+              account: mockStaffAccount,
+            }}
+          />
+          ;
+        </AuthProvider>
+      </QueryClientProvider>,
+    );
+
+    cy.contains("Consultation Records Information").should("exist");
+    cy.get("[data-testid='review-section']").should("be.visible");
+    cy.get("[data-testid='review-section']")
+      .find("input[type='radio'][value='YES']")
+      .should("have.length", 2)
+      .each(($radio) => {
+        cy.wrap($radio).should("be.checked");
+      });
+
+    cy.get("[data-testid='review-completed-notification']").should("exist");
+  });
+
+  it("test review section - staff", () => {
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
+
+    queryClient.setQueryData(
+      [QUERY_KEY.SUBMISSION_PACKAGE, mockSubmissionPackage.id],
+      mockSubmissionPackage,
+    );
+    queryClient.setQueryData(
+      [QUERY_KEY.ACCOUNT_PROJECT, mockAccountProject.id],
+      mockAccountProject,
+    );
+    queryClient.setQueryData(
+      [QUERY_KEY.SUBMISSION_ITEM, mockConsultationRecordItemPassed.id],
+      mockConsultationRecordItemPassed,
+    );
+
+    const router = createRouter({
+      routeTree: routeTree,
+      context: {
+        authentication: mockAuthentication,
+        queryClient: queryClient,
+        account: mockStaffAccount,
+      },
+    });
+
+    mockZustandStore(useAccount, {
+      userType: USER_TYPE.STAFF,
+      roles: [
+        EPIC_SUBMIT_ROLE.eao_create,
+        EPIC_SUBMIT_ROLE.eao_edit,
+        EPIC_SUBMIT_ROLE.eao_view,
+      ],
+      reset: () => {},
+    });
+
+    router.navigate({
+      to: `/staff/projects/${mockAccountProject.id}/submission-packages/${mockSubmissionPackage.id}/submissions/${mockConsultationRecordItemPassed.id}`,
+    });
+
+    mount(
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider {...OidcConfig}>
+          <RouterProvider
+            router={router}
+            context={{
+              authentication: mockAuthentication,
+              account: mockStaffAccount,
+            }}
+          />
+          ;
+        </AuthProvider>
+      </QueryClientProvider>,
+    );
+
+    cy.contains("Consultation Records Information").should("exist");
+    cy.get("[data-testid='review-section']").should("be.visible");
+    cy.get("[data-testid='review-section']")
+      .find("input[type='radio'][value='YES']")
+      .should("have.length", 1)
+      .each(($radio) => {
+        cy.wrap($radio).should("be.checked");
+      });
+
+    cy.get("[data-testid='review-completed-notification']").should("exist");
+  });
+});
