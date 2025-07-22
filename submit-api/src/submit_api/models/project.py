f66a9a6891dd98c862cfd4124c913fb624955424
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2025-07-22 08:47:17 -0700

    linting

--- submit-api/src/submit_api/models/project.py
+++ submit-api/src/submit_api/models/project.py
@@ -57,7 +57,7 @@ class Project(db.Model):
         proponents = (
             cls.query
             .with_entities(cls.proponent_id, cls.proponent_name)
-            .filter(cls.has_approved_condition == True)
+            .filter(cls.has_approved_condition is True)
             .distinct()
             .order_by(cls.proponent_name)
             .all()
