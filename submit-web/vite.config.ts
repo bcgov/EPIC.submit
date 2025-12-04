019ccf557c3f7211ce4675632e99927d4cf4a6fc
Dinesh <97143739+dinesh-aot@users.noreply.github.com>
2025-12-04 12:03:31 -0800

    changes related to the base path (#705)
    * relative path added

--- submit-web/vite.config.ts
+++ submit-web/vite.config.ts
@@ -6,6 +6,7 @@ import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_APP_BASE_PATH || "/",
   plugins: [
     TanStackRouterVite(),
     react(),
