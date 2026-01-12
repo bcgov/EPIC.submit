639d436281f66f63b861c9942e4bfd276b7bd66f
don-aot <don.varghese@aot-technologies.com>
2026-01-12 12:32:43 -0800

    SUBMIT-697:EAO - Proponents/Holders List
    Linting issue fixes

--- submit-api/src/submit_api/models/proponent.py
+++ submit-api/src/submit_api/models/proponent.py
@@ -33,23 +33,23 @@ class Proponent(db.Model):
     @classmethod
     def get_all_proponents(cls, include_deleted=False, approved_conditions_only=False):
         """Get all proponents.
-        
+
         Args:
             include_deleted: If True, includes deleted proponents. Defaults to False.
-            approved_conditions_only: If True, returns only proponents that have projects 
+            approved_conditions_only: If True, returns only proponents that have projects
                                      with approved conditions. Defaults to False.
-        
+
         Returns:
             List of Proponent objects ordered by name.
         """
         query = cls.query
-        
+
         if approved_conditions_only:
             query = query.join(Project, cls.id == Project.proponent_id)
             query = query.filter(Project.has_approved_condition.is_(True))
             query = query.distinct()
-        
+
         if not include_deleted:
             query = query.filter(cls.is_deleted.is_(False))
-        
+
         return query.order_by(cls.name).all()
