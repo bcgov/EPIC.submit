4026f23e49dbcdf5db8bfe99a0791669f2a07456
Dinesh <97143739+dinesh-aot@users.noreply.github.com>
2026-01-23 14:38:31 -0800

    SUBMIT-734 Helm charts and CronD lib updated (#765)
    * gitingore changes
    
    * helm changes cron and gocron library change

--- submit-cron/docker-entrypoint.sh
+++ submit-cron/docker-entrypoint.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Handle OpenShift random UID assignment
+# OpenShift assigns random UIDs at runtime which causes go-crond's user lookup to fail
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "default:x:$(id -u):0:default user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
 function start_cron_jobs() {
   echo "Starting go-crond as a background task ..."
   CRON_CMD="go-crond -v --allow-unprivileged --include=cron/"
