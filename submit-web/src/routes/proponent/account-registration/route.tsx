15c27f9d33e1755904cfeac3c5cbf7e83ef7a4ef
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2026-04-07 15:21:04 -0600

    SUBMIT-770: Proponent SignUp issue with existing account user

--- submit-web/src/routes/proponent/account-registration/route.tsx
+++ submit-web/src/routes/proponent/account-registration/route.tsx
@@ -2,6 +2,7 @@ import { EntityBanner } from "@/components/App/AccountRegistration/EntityBanner"
 import { useCreateAccountFormStore } from "@/components/App/AccountRegistration/formStore";
 // import SaveLaterFooter from "@/components/App/AccountRegistration/SaveLaterFooter";
 import { useGetProponent } from "@/hooks/api/useProponents";
+import { useAccount } from "@/store/accountStore";
 import { Box } from "@mui/material";
 import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router";
 import { useEffect } from "react";
@@ -14,12 +15,22 @@ function AccountRegistration() {
   const { invitation, setEntityName, completed } = useCreateAccountFormStore();
   const { data: proponent } = useGetProponent(invitation?.proponent_id ?? 0);
   const navigate = useNavigate();
+  const { userId, accountId } = useAccount();
 
   useEffect(() => {
     if (completed) {
       navigate({ to: "/proponent/projects", replace: true });
     }
   }, [completed, navigate]);
+
+  useEffect(() => {
+    if (userId && accountId) {
+      navigate({
+        to: "/proponent/account-registration/registration-error",
+        replace: true,
+      });
+    }
+  }, [userId, accountId, navigate]);
 
   useEffect(() => {
     if (proponent) {
