966e7184a1323dc200a6fb286ec21de21b8acd7f
dinesh <dinesh.pb@aot-technologies.com>
2026-04-23 18:37:36 -0700

    Previous Update Requests

--- submit-api/src/submit_api/models/update_request.py
+++ submit-api/src/submit_api/models/update_request.py
@@ -34,12 +34,15 @@ class UpdateRequest(BaseModel):
     __tablename__ = 'update_requests'
 
     id = Column(db.Integer, primary_key=True, autoincrement=True)
-    submission_package_id = Column(db.Integer, ForeignKey('packages.id', ondelete='CASCADE'), nullable=False)
-    submission_item_types = Column(db.ARRAY(db.Integer), nullable=False)
+    submission_package_id = Column(db.Integer, ForeignKey('packages.id', ondelete='CASCADE'), nullable=False,)
+    submission_item_types = Column(db.ARRAY(db.Integer), nullable=False, comment='Array of submission item type IDs')
     active = Column(db.Boolean, nullable=False, default=True)
     created_by = Column(db.String, ForeignKey('users.auth_guid'), nullable=False)
     created_by_user = db.relationship('User', foreign_keys=[created_by], lazy='joined')
     reason = Column(db.String, nullable=True)
     type = Column(Enum(UpdateRequestType), nullable=False, default=UpdateRequestType.UPDATE)
     note = Column(db.String, nullable=True)
+    note_updated_by = Column(db.String, ForeignKey('users.auth_guid'), nullable=True, comment='Proponent user who last updated the note')
+    note_updated_by_user = db.relationship('User', foreign_keys=[note_updated_by], lazy='joined')
+    note_updated_at = Column(db.DateTime, nullable=True, comment='Timestamp when the note was last updated')
     status = Column(db.String, nullable=False, default=UpdateRequestStatus.OPEN.value)
