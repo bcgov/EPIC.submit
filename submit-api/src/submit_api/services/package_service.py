05b486cc590dbd0e80e2530e2fca21e549936fa8
dinesh <dinesh.pb@aot-technologies.com>
2026-06-29 13:12:00 -0700

    Approval/Not Approval restriction by TEAM_LEAD

--- submit-api/src/submit_api/services/package_service.py
+++ submit-api/src/submit_api/services/package_service.py
@@ -235,8 +235,27 @@ class PackageService:
     @classmethod
     def get_package_by_id(cls, package_id):
         """Get package by id."""
+        from submit_api.models import StaffUserWork  # pylint: disable=import-outside-toplevel
+
         authorization.has_access_to_package(package_id)
         package = PackageModel.get_package_by_id_with_items(package_id)
+
+        # Attach work_role to account_project_work for staff users
+        if package and package.account_project_work:
+            user = User.get_by_guid(TokenInfo.get_username())
+            if user and user.type == UserType.STAFF and user.staff_user:
+                # Get the work role for this staff user and work
+                staff_user_work = StaffUserWork.query.filter_by(
+                    staff_user_id=user.staff_user.id,
+                    work_id=package.account_project_work.work_id,
+                    is_active=True
+                ).first()
+
+                if staff_user_work:
+                    package.account_project_work.current_user_work_role = staff_user_work.role
+                else:
+                    package.account_project_work.current_user_work_role = None
+
         return package
 
     @classmethod
@@ -816,7 +835,14 @@ class PackageService:
 
     @classmethod
     def approve_package(cls, package_id):
-        """Approve the package."""
+        """Approve the package.
+
+        Only Team Leads and users with full_access role can approve packages.
+        Team Members can view, verify, acknowledge, and request updates.
+        """
+        # Require Team Lead or full_access role for approval
+        authorization.require_team_lead_access()
+
         with session_scope() as session:
             package = cls.get_package_by_id(package_id)
             if not package:
