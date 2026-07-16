84cc1d58fc4f548b87d6b8b8229dbaa218d0d0f8
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2026-07-16 14:35:01 +0530

    Increase GIS upload test limit

--- submit-web/src/utils/constants.ts
+++ submit-web/src/utils/constants.ts
@@ -23,7 +23,7 @@ export const DEFAULT_MAX_FILE_SIZE_MB = 500;
 
 // Geospatial files (.shp/.zip) are capped lower because processing large
 // files is expensive and can time out. This limit applies to geospatial only.
-export const GEO_MAX_FILE_SIZE_MB = 20;
+export const GEO_MAX_FILE_SIZE_MB = 100;
 export const GEO_MAX_FILE_SIZE_BYTES = GEO_MAX_FILE_SIZE_MB * 1024 * 1024;
 
 export const EXTENSION_TO_MIME_TYPE_MAP: Record<string, string[]> = {
